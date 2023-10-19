a62c7444d4ad9b3874acfabbb99366ef717d76dd
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2023-10-19 20:58:04 +0530

    first commit

--- app/land.tsx
+++ app/land.tsx
@@ -17,10 +17,10 @@ export default function Land(p) {
     .then(json=>setdata(json))
     .catch((er)=>{console.log(er);
     })
-  },[])
+  })
   return (
 data&&<div className='flex  flex-auto overflow-x-scroll overflow-hidden '>
-{data.map((data:any)=>(<div ><Card data={data} /></div>))}
+{data.map((data)=>(<div key={data.data.id} ><Card data={data} /></div>))}
 </div>
   )
   }
