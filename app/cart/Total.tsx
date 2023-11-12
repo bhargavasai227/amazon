4730adefab2066b8b244c5097d7a9dac4977497e
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2023-11-12 08:11:55 +0530

    rectified key attribute

--- app/cart/Total.tsx
+++ app/cart/Total.tsx
@@ -11,7 +11,7 @@ const Total = () => {
 
   return <div className="fixed bottom-0 w-full flex ">
    <div className=" m-3 w-3/4 bg-gray-800 p-4 text-white rounded-md">
-    <div>{items.map((e:{title:string,price:string,index:number})=>(<div className="flex gap-2"><span>{e.title}</span><p>${e.price}</p></div>))}
+    <div>{items.map((e:{title:string,price:string,id:number})=>(<div key={e.id} className="flex gap-2"><span>{e.title}</span><p>${e.price}</p></div>))}
     <div className="py-4">Total Amount:  ${totalAmount}</div></div>
     </div>
 <div className="rounded-[2.2rem] w-32 h-16 max-h-24 relative top-11 bg-orange-500 m-11 p-4 ">pay using razorpay</div>
