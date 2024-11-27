9afbd59b748c7750ece957873c06c885f8b5ce8d
Bhargav Dasari <112853876+bhargavasai227@users.noreply.github.com>
2024-11-27 07:49:00 +0530

    Update Strip.tsx

--- app/cart/Strip.tsx
+++ app/cart/Strip.tsx
@@ -17,5 +17,5 @@ async function handleCheckout() {
   }
 
   export default function Stripe(){
-    return <button className='rounded-[2.2rem] w-32 h-16 max-h-24 relative  bottom-[-3] bg-orange-500 m-11 p-4 ' onClick={handleCheckout}>{window.location.href}</button>
+    return <button className='rounded-[2.2rem] w-32 h-16 max-h-24 relative  bottom-[-3] bg-orange-500 m-11 p-4 ' onClick={handleCheckout}>Pay</button>
   }
